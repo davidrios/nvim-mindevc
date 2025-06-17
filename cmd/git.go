9845d1805a836dd4177a6690fade20fe3f90f16f
David Rios <david.rios.gomes@gmail.com>
2025-06-17 09:30:57 -0300

    Implement git --version

--- cmd/git.go
+++ cmd/git.go
@@ -1,14 +1,22 @@
 package cmd
 
 import (
+	"fmt"
 	"log/slog"
 
 	"github.com/spf13/cobra"
 )
 
+var gitShowVersion bool
+
 var gitCmd = &cobra.Command{
 	Use:   "git",
 	Short: "Minimal git implementation",
+	Run: func(cmd *cobra.Command, args []string) {
+		if gitShowVersion {
+			fmt.Println("git version 2.38.5")
+		}
+	},
 }
 
 func init() {
@@ -19,6 +27,12 @@ func init() {
 		"verbose", "v",
 		false,
 		"show debug messages")
+
+	gitCmd.Flags().BoolVar(
+		&gitShowVersion,
+		"version",
+		false,
+		"")
 
 	if verbose {
 		slog.SetLogLoggerLevel(slog.LevelDebug)
